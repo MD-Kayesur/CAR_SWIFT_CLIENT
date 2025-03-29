44a128ac7dbb4a6348a3b38d9d1e7c6b815a2e44
MD-Kayesur <rmdkayesur@gmail.com>
2025-03-29 09:31:36 -0600

    done

--- src/main.jsx
+++ src/main.jsx
@@ -6,20 +6,15 @@ import { RouterProvider } from "react-router-dom";
 import MainLayout from "./Layouts/MainLayout.jsx";
 import { router } from "./Routes/Router.jsx";
 import AuthProviders from "./authentication/AuthProvider.jsx";
-import {  QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import PrivateRoute from "./ProivetRout/PrivateRoute.jsx";
 const quearyClient = new QueryClient();
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-<QueryClientProvider client={quearyClient}>
-
-<AuthProviders>
- 
-      <RouterProvider router={router} />
- 
-    </AuthProviders>
-</QueryClientProvider>
-
-   
+    <QueryClientProvider client={quearyClient}>
+      <AuthProviders>
+        <RouterProvider router={router} />
+      </AuthProviders>
+    </QueryClientProvider>
   </StrictMode>
 );
